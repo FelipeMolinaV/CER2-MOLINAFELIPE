1692661d5cc41cc1c356c24d8d46c2ce7cf2c0ac
Felipe Molina <fmolinav@usm.cl>
2025-10-25 01:24:31 -0300

    Se crea el modelo Evento y ImagenEvento, se hacen las relaciones muchos a muchos entre eventos e imagenEvento y evento con usuario

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ImagenEvento(models.Model):
+    url = models.URLField(max_length=300)
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=35)
+    fecha = models.DateTimeField()
+    lugar = models.CharField(max_length=35)
+    url = models.ManyToManyField(ImagenEvento)
+    valor = models.PositiveIntegerField()
+    valor = models.PositiveBigIntegerField()
+    registrados = models.ManyToManyField(User)
+
+
