15e3c8c9eb666dabd5a80c76103d7fd0772fce18
Felipe Molina <fmolinav@usm.cl>
2025-10-25 05:03:52 -0300

    Se a mejorado el template evento/id y ademas se ha creado el comportamiento en el sitio para un usuario anonimo

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.principal, name="principal"),
     path('evento/<int:id>', views.informacion_evento, name="informacion_evento"),
+    path('crear/', views.crear_cuenta, name="crear_cuenta"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import Evento, ImagenEvento
 
 def principal(request):
@@ -10,4 +12,23 @@ def principal(request):
 def informacion_evento(request, id):
     return render(request, 'core/informacion_evento.html', {
         'evento': Evento.objects.get(id=id),
+        'cupos': (Evento.objects.get(id=id).cantidad - Evento.objects.get(id=id).registrados.all().count())
     })
+
+def crear_cuenta(request):
+    if request.method == "POST":
+
+        nombre_usuario = request.POST['nombre']
+        correo = request.POST['correo']
+        contraseña = request.POST['contraseña']
+        
+        user = User.objects.create_user(username=nombre_usuario, email=correo, password=contraseña)
+        user.save()
+
+        usuario = authenticate(username=nombre_usuario, password=contraseña)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('principal')
+
+    else:  
+        return render(request, 'core/crear_cuenta.html', {})
